fe3c203c38849f66fd83b216cb3cf654b0b81468
Dr. Raed <drraed.jb@gmail.com>
2025-06-09 01:20:54 +0300

    Add

--- project10/settings.py
+++ project10/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q_4!m1z1agdls014pbq_ikfdtf7xmkha5xb$5o@qh2cf5^kdvm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost",]
 
 
 # Application definition
